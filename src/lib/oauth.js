cd06c6ad474517cdf1deb5cfde078dbadb7b54e5
byrne.reese <byrne@majordojo.com>
2022-01-03 12:26:02 -0800

    Fixed backtick bug

--- src/lib/oauth.js
+++ src/lib/oauth.js
@@ -6,7 +6,7 @@ const oauthApp = new ClientOAuth2({
     clientId:         process.env.AHA_CLIENT_ID,
     clientSecret:     process.env.AHA_CLIENT_SECRET,
     redirectUri:      `${process.env.RINGCENTRAL_CHATBOT_SERVER}/aha/oauth`,
-    accessTokenUri:   'https://${process.env.AHA_SUBDOMAIN}.aha.io/oauth/token',
+    accessTokenUri:   `https://${process.env.AHA_SUBDOMAIN}.aha.io/oauth/token`,
     authorizationUri: `https://${process.env.AHA_SUBDOMAIN}.aha.io/oauth/authorize?client_id=${process.env.AHA_CLIENT_ID}&redirect_uri=${process.env.RINGCENTRAL_CHATBOT_SERVER}/bot/oauth&response_type=code`,
     scopes: ''
 });
