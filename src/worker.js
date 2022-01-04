6909708a3824a998ef0f5697a2c5c9362c756a25
byrne.reese <byrne@majordojo.com>
2022-01-04 15:20:20 -0800

    Updated worker to begin summarizing changes

--- src/worker.js
+++ src/worker.js
@@ -11,6 +11,8 @@ let REDIS_URL = process.env.REDIS_URL || "redis://127.0.0.1:6379";
 // See: https://devcenter.heroku.com/articles/node-concurrency for more info
 let workers = process.env.WEB_CONCURRENCY || 2;
 
+let IGNORE_FIELDS = new RegExp('(Created by user|Rank|Assigned to user|Show feature remaining estimate|Reference num)')
+
 // The maximum number of jobs each worker should process at once. This will need
 // to be tuned for your application. If each job is mostly waiting on network 
 // responses it can be much higher. If each job is CPU-intensive, it might need
@@ -23,35 +25,83 @@ function start() {
     let workQueue = new Queue('work', REDIS_URL);
 
     workQueue.process(maxJobsPerWorker, async (job) => {
-	console.log(`WORKER: job.group_id = ${job.data.group_id}`)
-	console.log(`WORKER: job.bot_id = ${job.data.bot_id}`)
-	console.log(`WORKER: job.aha_type = ${job.data.aha_type}`)
-	console.log(`WORKER: job.aha_id = ${job.data.aha_id}`)
+	console.log(`WORKER: processing ${job.data.aha_type}:${job.data.aha_id}`)
 
 	let progress = 0;
-	const changes = await ChangesModel.findAll({
+	const accumulated_changes = await ChangesModel.findAll({
 	    'where': {
 		'ahaType' : job.data.aha_type,
 		'ahaId'   : job.data.aha_id
 	    }
 	})
-	if (changes) {
-	    // Changes were found. Make a note, then delete the change.
-	    // Remember, at this point we are not yet doing anything.
-	    // TODO - do something
-	    for (let i = 0; i < changes.length; i++)  {
-		let change = changes[i]
+	if (accumulated_changes) {
+	    console.log(`${changes.length} found to aggregate`);
+	    let changed_fields = {}
+	    let aha_object = {}
+	    
+	    // Aggregate and summarize the changes received. 
+	    for (let i = 0; i < accumuluated_changes.length; i++)  {
+		let audit = JSON.parse( accumulated_changes[i].data )
+		if (i == 0) {
+		    aha_object[ 'id' ]         = audit.auditable_id
+		    aha_object[ 'type' ]       = audit.auditable_type
+		    aha_object[ 'url' ]        = audit.auditable_url
+		    aha_object[ 'created_at' ] = audit.created_at
+		}
+		for (var j in audit.changes) {
+                    let change = audit.changes[j]
+
+		    // Figure out what changes we want to skip/ignore
+		    // Duplicates are ok, because we will just use the most recent value
+		    if (change.value == '' || // empty value
+			(IGNORE_FIELDS.test(change.field_name) && audit.audit_action === "create")
+                       ) {
+			console.log(`Skipping changes to field ${change.field_name}`)
+			continue
+                    }
+
+		    // Format the value we will set the field to
+                    let change_value = ''
+                    if (audit.auditable_type === "note" ||
+			change.field_name.includes("Comment by")) {
+			change_value = turnDown.turndown(change.value.toString())
+                    } else {
+			change_value = entities.decode(change.value.toString())
+                    }
+
+		    // Add the change to the struct were we are storing all aggregated changes
+		    changed_fields[ change.field_name ] = change_value;
+		}
+		// delete the change now that we have aggregated it successfully
 		console.log(`WORKER: Deleting change: ${change.id}`);
 		await ChangesModel.destroy({
 		    'where': { 'id': change.id }
 		})
 	    }
+	    // end aggregation for loop
+
+	    // Send an adaptive card summarizing the changes
+            const cardData = {
+                actionTitle: `${job.data.aha_type} updated`,
+                actionText: `The following fields were modified ${aha_object['url']}`,
+                changes: changed_fields,
+                footNote: `Changes made by TODO at TODO}`
+            }
+	    console.log("Card data that will be posted: ", cardData)
+            //const template = new Template(ahaCardTemplate);
+            //const card = template.expand({
+            //    $root: cardData
+            //});
+            //await bot.sendAdaptiveCard(groupId, card);
+	    // End sending of adaptive card
+	    
+	} else {
+	    console.log(`No changes were found to aggregate. This technically shouldn't happen.`);
 	}
-    
 		      
 	// A job can return values that will be stored in Redis as JSON
 	// This return value is unused in this demo application.
-	return { value: "This will be stored" };
+	return { value: `${changes.length} aggregated for ${job.data.aha_type} with id of ${job.data.aha_id}.` };
     });
 }
 
