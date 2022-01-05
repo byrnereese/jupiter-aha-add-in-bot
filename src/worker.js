db0d2dd92c474a96c72baf1e23911c024efc4aaf
byrne.reese <byrne@majordojo.com>
2022-01-05 01:31:08 -0800

    fixed another variable name reference

--- src/worker.js
+++ src/worker.js
@@ -113,7 +113,7 @@ function start() {
                 $root: cardData
             });
 	    console.log("WORKER: posting card:", card)
-            await bot.sendAdaptiveCard(groupId, card);
+            await bot.sendAdaptiveCard( job.data.group_id, card);
 	    console.log("WORKER: card posted")
 	    // End sending of adaptive card
 	    
