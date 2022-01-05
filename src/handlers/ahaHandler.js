6f9b092888f0f8d59294add21354b2957681964c
byrne.reese <byrne@majordojo.com>
2022-01-05 00:43:45 -0800

    Updated logging

--- src/handlers/ahaHandler.js
+++ src/handlers/ahaHandler.js
@@ -47,7 +47,7 @@ const ahaWebhookHandler = async (req, res) => {
         return
     }
     console.log(`Received webhook from Aha (group: ${groupId}, bot: ${botId})...`)
-    console.log(`Whole webhook:`, req.body)
+    console.log(`Whole webhook:`, req)
 
     let audit = req.body.audit
     let webhook_data = JSON.stringify(audit, null, 2);
