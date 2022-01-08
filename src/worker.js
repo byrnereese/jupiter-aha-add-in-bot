3e9e40b33caab76425b12f75d35bde8956304c0c
byrne.reese <byrne@majordojo.com>
2022-01-08 00:39:05 -0800

    fixed turndown for description

--- src/worker.js
+++ src/worker.js
@@ -62,7 +62,7 @@ function start() {
 			let states = []
 			let promise2 = aha.product.ideaCategories( productId, function (err, data, response) {
 			    //console.log("WORKER: categories fetched from aha: ", data)
-			    let desc = turnDown.turndown( idea.description )
+			    let desc = turnDown.turndown( idea.description.body.toString() )
 			    const cardData = {
 				ahaId: job.data.audit.auditable_id,
 				ahaUrl: job.data.audit.auditable_url,
